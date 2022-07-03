a4e71180a82f7dfc6634f84a50e882bec264db10
Jody VanHoose <jodyvanhoose80@gmail.com>
2022-07-03 13:47:10 -0400

    fix maybe

--- src/index.test.js
+++ src/index.test.js
@@ -11,7 +11,7 @@ describe('Our first test', () => {
 describe('index.html', () => {
   it('should say hello', () => {
     const index = fs.readFileSync("./src/index.html", "utf-8")
-    const { JSDOM } = jsdom
+    const { JSDOM } = jsdom;
     const dom = new JSDOM(index)
     const h1 = dom.window.document.getElementsByTagName("h1")[0]
     expect(h1.innerHTML).to.equal("Hello World!")
